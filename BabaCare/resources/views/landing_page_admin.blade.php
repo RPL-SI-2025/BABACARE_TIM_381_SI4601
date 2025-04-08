f5bcf55e0d0b02e235425fda4509dc3302252fd3
SadewaCahya <sadewacahya90@gmail.com>
2025-04-08 09:26:42 +0700

    BABACARE-1

--- BabaCare/resources/views/landing_page_admin.blade.php
+++ BabaCare/resources/views/landing_page_admin.blade.php
@@ -9,7 +9,7 @@
     <title>BabaCare - Landing Page Petugas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</head>
+</head> 
 <body style="background-color: #F7F8FD;">
     <div class="container-fluid">
         <div class="row">
